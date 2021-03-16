7ff77393ca14c0a810631caed33321339779d5fb
PhiApp <philipp.appenauer@sz-ybbs.ac.at>
2021-03-17 00:03:24 +0100

    adopt template

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# szdevman-elm
-a elm frontend client for the szdevman project
+# Elm 0.19.1 with Webpack 4, Hot Reloading & Babel 7
+
+Elm dev environment with hot-loading (i.e. state is retained as you edit your code - Hot Module Reloading, HMR)).
+
+## Installation
+
+Clone this repo into a new project folder and run install script.
+(You will probably want to delete the .git/ directory and start version control afresh.)
+
+With npm
+
+```sh
+$ git clone git@github.com:PhiApp/szdevman-elm.git
+$ cd szdevman-elm
+$ npm install
+```
+
+## Visual Studio Code Extension
+
+Install the Visual Studio Code Extension for language support.
+
+https://marketplace.visualstudio.com/items?itemName=Elmtooling.elm-ls-vscode
+
+## Developing
+
+Start with Elm debug tool with either
+```sh
+$ npm start
+or
+$ npm start --nodebug
+```
+
+the `--nodebug` removes the Elm debug tool. This can become valuable when your model becomes very large.
+
+Open http://localhost:3000 and start modifying the code in /src.  **Note** that this starter expects you have installed [elm-format globally](https://github.com/avh4/elm-format#installation-). 
+
+An example using Routing is provided in the `navigation` branch
+
+## Production
+
+Build production assets (js and css together) with:
+
+```sh
+npm run prod
+```
+
+## Static assets
+
+Just add to `src/assets/` and the production build copies them to `/dist`
+
+## Testing
+
+[Install elm-test globally](https://github.com/elm-community/elm-test#running-tests-locally)
+
+`elm-test init` is run when you install your dependencies. After that all you need to do to run the tests is
+
+```
+npm test
+```
+
+Take a look at the examples in `tests/`
+
+If you add dependencies to your main app, then run `elm-test --add-dependencies`
+
+<!-- I have also added [elm-verify-examples](https://github.com/stoeffel/elm-verify-examples) and provided an example in the definition of `add1` in App.elm. -->
+
+## Elm-analyse
+
+Elm-analyse is a "tool that allows you to analyse your Elm code, identify deficiencies and apply best practices." Its built into this starter, just run the following to see how your code is getting on:
+
+```sh
+$ npm run analyse
+```
+
+## ES6
+
+This starter includes [Babel](https://babeljs.io/) so you can directly use ES6 code.
+
+ ## How it works
+
+`npm run dev` maps to `webpack-dev-server --hot --colors --port 3000` where
+
+  - `--hot` Enable webpack's Hot Module Replacement feature
+  - `--port 3000` - use port 3000 instead of default 8000
+  - inline (default) a script will be inserted in your bundle to take care of reloading, and build messages will appear in the browser console.
+  - `--colors` should show the colours created in the original Elm errors, but does not (To Fix)
+  
+One alternative is to run `npx webpack-dev-server --hot --colors --host=0.0.0.0 --port 3000` which will enable your dev server to be reached from other computers on your local network
+
+ ## Credits
+
+ Thanks to simonh1000 for creating the elm elm-live webpack template 
+ https://github.com/simonh1000/elm-webpack-starter
---
+++ src/Main.elm
@@ -0,0 +1,204 @@
+port module Main exposing (Model, Msg(..), add1, init, main, toJs, update, view)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Http exposing (Error(..))
+import Json.Decode as Decode
+import Html.Attributes
+
+
+
+-- ---------------------------
+-- PORTS
+-- ---------------------------
+
+
+port toJs : String -> Cmd msg
+
+
+
+-- ---------------------------
+-- MODEL
+-- ---------------------------
+
+
+type alias Model =
+    { counter : Int
+    , serverMessage : String
+    }
+
+
+init : Int -> ( Model, Cmd Msg )
+init flags =
+    ( { counter = flags, serverMessage = "" }, Cmd.none )
+
+
+
+-- ---------------------------
+-- UPDATE
+-- ---------------------------
+
+
+type Msg
+    = Inc
+    | TestServer
+    | OnServerResponse (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        Inc ->
+            ( add1 model, toJs "Inc" )
+
+        TestServer ->
+            let
+                expect =
+                    Http.expectJson OnServerResponse (Decode.field "result" Decode.string)
+            in
+            ( model
+            , Http.get { url = "/test", expect = expect }
+            )
+
+        OnServerResponse res ->
+            case res of
+                Ok r ->
+                    ( { model | serverMessage = r }, Cmd.none )
+
+                Err err ->
+                    ( { model | serverMessage = "Error: " ++ httpErrorToString err }, Cmd.none )
+
+
+httpErrorToString : Http.Error -> String
+httpErrorToString err =
+    case err of
+        BadUrl url ->
+            "BadUrl: " ++ url
+
+        Timeout ->
+            "Timeout"
+
+        NetworkError ->
+            "NetworkError"
+
+        BadStatus _ ->
+            "BadStatus"
+
+        BadBody s ->
+            "BadBody: " ++ s
+
+
+{-| increments the counter
+
+    add1 5 --> 6
+
+-}
+add1 : Model -> Model
+add1 model =
+    { model | counter = model.counter + 1 }
+
+
+
+-- ---------------------------
+-- VIEW
+-- ---------------------------
+
+
+view : Model -> Html Msg
+view model =
+    div [] 
+    [
+        div [ class "pure-menu pure-menu-horizontal" ]
+            [ a 
+                [ class "pure-menu-heading pure-menu-link"
+                , href "#"
+                ]
+                [ text "SzDevMan" ]
+            , ul [ class "pure-menu-list" ] 
+            [ li [ class "pure-menu-list" ]
+                [ a 
+                    [ class "pure-menu-link"
+                    , href "#"
+                    ]
+                    [ text "Register" 
+                    ]
+                ]
+                , li [ class "pure-menu-list" ]
+                [ a 
+                    [ class "pure-menu-link"
+                    , href "#"
+                    ]
+                    [ text "UserList" 
+                    ]
+                ]
+            ]
+        ]
+        ,  div [ class "container" ]
+        [ header [ class "pure-g" ]
+            [ h1 [ class "pure-u-4-5" ] [ text "Elm 0.19.1 Webpack Starter, with hot-reloading" ]
+            ]
+        , p [] [ text "Click on the button below to increment the state." ]
+        , div [ class "pure-g" ]
+            [ div [ class "pure-u-1-3" ]
+                [ button
+                    [ class "pure-button pure-button-primary"
+                    , onClick Inc
+                    ]
+                    [ text "+ 1" ]
+                , text <| String.fromInt model.counter
+                ]
+            , div [ class "pure-u-1-3" ] []
+            , div [ class "pure-u-1-3" ]
+                [ button
+                    [ class "pure-button pure-button-primary"
+                    , onClick TestServer
+                    ]
+                    [ text "ping dev server" ]
+                , text model.serverMessage
+                ]
+            ]
+        , p [] [ text "Then make a change to the source code and see how the state is retained after recompilation." ]
+        , p []
+            [ text "And now don't forget to add a star to the Github repo "
+            , a [ href "https://github.com/simonh1000/elm-webpack-starter" ] [ text "elm-webpack-starter" ]
+            ]
+        , Html.form [ class "pure-form pure-form-stacked"] 
+        [ fieldset [] 
+            [ legend [] [ text "Bitte gebe deine Benutzerdaten ein:" ]
+            , label [ for "stacked-email" ] [ text "Email" ]
+            , input [ Html.Attributes.type_ "email"
+                , id "stacked-email"
+                , placeholder "Email"
+                ] []
+            , span [class "pure-form-message"] [ text "This is a required field." ]
+            , label [ for "stacked-password" ] [ text "Password" ]
+            , input [ Html.Attributes.type_ "password"
+                , id "stacked-password"
+                , placeholder "Password" ] []
+            ]
+            , button [ Html.Attributes.type_ "submit"
+                , class "pure-button pure-button-primary" ] [ text "Sign in"]
+            ] 
+        ]
+    ]
+
+
+-- ---------------------------
+-- MAIN
+-- ---------------------------
+
+
+main : Program Int Model Msg
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , view =
+            \m ->
+                { title = "szdevman :: register"
+                , body = [ view m ]
+                }
+        , subscriptions = \_ -> Sub.none
+        }
