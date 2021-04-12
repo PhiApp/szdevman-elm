6970996f11ee3f09587ef5b104a9c433c13c37e3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-12 16:16:49 +0200

    implement user creation logic

--- src/Pages/UserRegister.elm
+++ src/Pages/UserRegister.elm
@@ -1,15 +1,17 @@
 port module Pages.UserRegister exposing (..)
 
+import Browser exposing (UrlRequest)
+import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Http exposing (Error(..))
-
+import Http exposing (..)
+import Json.Decode as Decode exposing (null)
+import Json.Encode as Encode
 import Url
 import Url.Parser as Parser
-import Browser.Navigation as Nav
-import Browser exposing (UrlRequest)
-import Json.Decode as Decode
+import Debug exposing (toString)
+
 
 
 -- ---------------------------
@@ -19,6 +21,8 @@ import Json.Decode as Decode
 
 port toJs : String -> Cmd msg
 
+
+
 -- ---------------------------
 -- MODEL
 -- ---------------------------
@@ -26,25 +30,50 @@ port toJs : String -> Cmd msg
 
 init : Int -> Nav.Key -> ( Model, Cmd Msg )
 init flags key =
-    ( {navKey= key, counter = flags, serverMessage = "" }, Cmd.none )
+    ( { navKey = key, counter = flags, serverMessage = "", httpError = None, email = "", password = "", name = "" }, Cmd.none )
 
 
-
-
-type alias Model = 
+type alias Model =
     { navKey : Nav.Key
     , counter : Int
     , serverMessage : String
+    , name : String
+    , email : String
+    , password : String
+    , httpError : HttpNotification
     }
-    
+
 -- ---------------------------
 -- UPDATE
 -- ---------------------------
+
 
 type Msg
     = Inc
     | TestServer
     | OnServerResponse (Result Http.Error String)
+
+
+type HttpNotification
+    = None
+    | Success
+    | Error
+
+
+postUser user =
+    Http.post
+        { url = "/api/users/"
+        , body = Http.jsonBody (userEncode user)
+        , expect = Http.expectJson OnServerResponse (Decode.field "result" Decode.string)
+        }
+
+
+userEncode user =
+    Encode.object
+        [ ( "Name", Encode.string user.name )
+        , ( "Email", Encode.string user.email )
+        , ( "Password", Encode.string user.password )
+        ]
 
 
 update message model =
@@ -53,22 +82,15 @@ update message model =
             ( add1 model, toJs "Inc" )
 
         TestServer ->
-            let
-                expect =
-                    Http.expectJson OnServerResponse (Decode.field "result" Decode.string)
-            in
-            ( model
-            , Http.get { url = "/test", expect = expect }
-            )
+            ( model, postUser { name = "awd", email = "awd", password = "" } )
 
         OnServerResponse res ->
             case res of
                 Ok r ->
-                    ( { model | serverMessage = r }, Cmd.none )
+                    ( { model | httpError = Success, serverMessage = "User created!" }, Cmd.none )
 
                 Err err ->
-                    ( { model | serverMessage = "Error: " ++ httpErrorToString err }, Cmd.none )
-
+                    ( { model | serverMessage = "Error: " ++ httpErrorToString err, httpError = Error }, Cmd.none )
 
 
 httpErrorToString : Http.Error -> String
@@ -83,79 +105,79 @@ httpErrorToString err =
         NetworkError ->
             "NetworkError"
 
-        BadStatus _ ->
-            "BadStatus"
+        BadStatus status ->
+            "BadStatus: " ++ toString status
 
         BadBody s ->
             "BadBody: " ++ s
 
 
-{-| increments the counter
-
-    add1 5 --> 6
-
--}
 add1 : Model -> Model
 add1 model =
     { model | counter = model.counter + 1 }
+
 
 
 -- ---------------------------
 -- VIEW
 -- ---------------------------
 
-view: Model -> Html Msg
+
+view : Model -> Html Msg
 view model =
-    div [] 
-    [   div [ class "container" ]
-        [ header [ class "pure-g" ]
-            [ h1 [ class "pure-u-4-5" ] [ text "Elm 0.19.1 Webpack Starter, with hot-reloading" ]
-            ]
-        , p [] [ text "Click on the button below to increment the state." ]
-        , div [ class "pure-g" ]
-            [ div [ class "pure-u-1-3" ]
-                [ button
-                    [ class "pure-button pure-button-primary"
-                    , onClick Inc
-                    ]
-                    [ text "+ 1" ]
-                , text <| String.fromInt model.counter
+    div []
+        [ div [ class "container" ]
+            [ Html.header [ class "pure-g" ]
+                [ h1 [ class "pure-u-4-5" ] [ text "User register" ]
                 ]
-            , div [ class "pure-u-1-3" ] []
-            , div [ class "pure-u-1-3" ]
-                [ button
-                    [ class "pure-button pure-button-primary"
-                    , onClick TestServer
+            , div
+                [ class "notification"
+                , classList
+                    [ ( "success", model.httpError == Success )
+                    , ( "error", model.httpError == Error )
                     ]
-                    [ text "ping dev server" ]
-                , text model.serverMessage
                 ]
+                [ text model.serverMessage ]
+            , Html.form
+                [ class "pure-form pure-form-stacked"
+                ]
+                [ fieldset []
+                    [ legend [] [ text "Please enter your username:" ]
+                    , label [ for "stacked-name" ] [ text "Name" ]
+                    , input
+                        [ Html.Attributes.type_ "text"
+                        , id "stacked-name"
+                        , placeholder "Name"
+                        , name "Name"
+                        ]
+                        []
+                    , label [ for "stacked-email" ] [ text "Email" ]
+                    , input
+                        [ Html.Attributes.type_ "email"
+                        , id "stacked-email"
+                        , placeholder "Email"
+                        , name "Email"
+                        ]
+                        []
+                    , span [ class "pure-form-message" ] [ text "This is a required field." ]
+                    , label [ for "stacked-password" ] [ text "Password" ]
+                    , input
+                        [ Html.Attributes.type_ "password"
+                        , id "stacked-password"
+                        , placeholder "Password"
+                        , name "Password"
+                        ]
+                        []
+                    ]
+                ]
+            , button
+                [ Html.Attributes.type_ "submit"
+                , class "pure-button pure-button-primary"
+                , onClick TestServer
+                ]
+                [ text "Sign in" ]
             ]
-        , p [] [ text "Then make a change to the source code and see how the state is retained after recompilation." ]
-        , p []
-            [ text "And now don't forget to add a star to the Github repo "
-            , a [ href "https://github.com/simonh1000/elm-webpack-starter" ] [ text "elm-webpack-starter" ]
-            ]
-        , Html.form [ class "pure-form pure-form-stacked"] 
-        [ fieldset [] 
-            [ legend [] [ text "Bitte gebe deine Benutzerdaten ein:" ]
-            , label [ for "stacked-email" ] [ text "Email" ]
-            , input [ Html.Attributes.type_ "email"
-                , id "stacked-email"
-                , placeholder "Email"
-                ] []
-            , span [class "pure-form-message"] [ text "This is a required field." ]
-            , label [ for "stacked-password" ] [ text "Password" ]
-            , input [ Html.Attributes.type_ "password"
-                , id "stacked-password"
-                , placeholder "Password" ] []
-            ]
-            , button [ Html.Attributes.type_ "submit"
-                , class "pure-button pure-button-primary" ] [ text "Sign in"]
-            ] 
         ]
-       
-    ]
 
 
 
@@ -163,5 +185,6 @@ view model =
 -- EXPORT
 -- ---------------------------
 
-toNavKey model = 
+
+toNavKey model =
     model.navKey
